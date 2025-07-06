0c425b069eb6bb0e01af1a898ea6cdbf61ecd885
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-07-06 08:25:51 +0200

    Moved module update service configuration to a separate class

---
+++ src/Configuration/ModuleUpdateServiceConfiguration.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2025 webtrees development team
+ *                    <http://webtrees.net>
+ *
+ * CustomModuleManager (webtrees custom module):
+ * Copyright (C) 2025 Markus Hemprich
+ *                    <http://www.familienforschung-hemprich.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * 
+ * CustomModuleManager
+ *
+ * A weebtrees(https://webtrees.net) 2.2 custom module to manage custom modules
+ * 
+ */
+
+declare(strict_types=1);
+
+namespace Jefferson49\Webtrees\Module\CustomModuleManager\Configuration;
+
+
+/**
+ * Configuration of the module update services
+ */
+class ModuleUpdateServiceConfiguration 
+{
+    //The configuration for the module update services
+    private const MODULE_UPDATE_SERVICE_CONFIG = [
+
+        '_change_language_with_url_'      =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/ChangeLanguageWithURL']],
+        '_extended_import_export_'        =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/ExtendedImportExport']],
+        '_oauth2_client_'                 =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/webtrees-oauth2-client']],
+        '_repository_hierarchy_'          =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/RepositoryHierarchy']],
+
+        '_jc-fancy-imagebar'              =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-fancy-imagebar']],
+        '_jc-fancy-research-links_'       =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-fancy-research-links']],
+        '_jc-fancy-treeview_'             =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-fancy-treeview']],
+        '_webtrees-simple-footer_'        =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-simple-footer']],
+        '_webtrees-simple-media-display_' =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-simple-media-display']],
+        '_webtrees-simple-menu_'          =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-simple-menu']],
+        '_jc-theme-justlight_'            =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'JustCarmen/webtrees-theme-justlight']],    
+
+        '_webtrees-primer-theme_'         =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'ekdahl/webtrees-primer-theme']],
+
+        '_GVExport_'                      =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Neriderc/GVExport']],
+
+        '_webtrees-descendants-chart_'    =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'magicsunday/webtrees-descendants-chart']],
+        '_webtrees-fan-chart_'            =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'magicsunday/webtrees-fan-chart']],
+        '_webtrees-pedigree-chart_'       =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'magicsunday/webtrees-pedigree-chart']],
+
+        '_myartjaub_ruraltheme_'          =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'jon48/webtrees-theme-rural']],
+
+        '_huhwt-cce_'                     =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-cce']],
+        '_huhwt-xtv_'                     =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-xtv']],
+        '_huhwt-wttam_'                   =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-wttam']],
+        '_huhwt-wtlin_'                   =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-wtlin']],
+        '_huhwt-tsm_'                     =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-tsm']],
+        '_huhwt-mtv_'                     =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'huhwt/huhwt-mtv']],
+
+        '_vesta_classic_look_and_feel_'   =>  ['update_service' => 'VestaModuleUpdate', 'params' => ['github_repo' => 'vesta-webtrees-2-custom-modules/vesta_classic_laf']],
+        '_vesta_clippings_cart_'          =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+        '_vesta_common_'                  =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+        '_vesta_extended_relationships_'  =>  ['update_service' => 'VestaModuleUpdate', 'params' => ['github_repo' => 'vesta-webtrees-2-custom-modules/vesta_extended_relationships']],
+        '_vesta_personal_facts_'          =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+        '_vesta_relatives_'               =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+        '_vesta_gov4webtrees_'            =>  ['update_service' => 'VestaModuleUpdate', 'params' => ['github_repo' => 'vesta-webtrees-2-custom-modules/vesta_gov4webtrees']],
+        '_vesta_places_and_pedigree_map_' =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+        '_vesta_research_suggestions_'    =>  ['update_service' => 'VestaModuleUpdate', 'params' => ['github_repo' => 'vesta-webtrees-2-custom-modules/vesta_research_suggestions']],
+        '_vesta_shared_places_'           =>  ['update_service' => 'VestaModuleUpdate', 'params' => ['github_repo' => 'vesta-webtrees-2-custom-modules/vesta_shared_places']],
+        '_vesta_location_data_'           =>  ['update_service' => 'VestaModuleUpdate', 'params' => []],
+    ];
+
+
+    /**
+     * Get the configuration for the module updates services
+     * 
+     * @return array
+     */
+    public static function getModuleUpdateServiceConfiguration(): array
+    {
+        return self::MODULE_UPDATE_SERVICE_CONFIG;
+    }    
+}
