946ec6747a942060c5e7c1c85305563616d9b20c
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-07-13 09:27:33 +0200

    Added Custom Module Manager to modules list

--- src/Configuration/ModuleUpdateServiceConfiguration.php
+++ src/Configuration/ModuleUpdateServiceConfiguration.php
@@ -46,6 +46,7 @@ class ModuleUpdateServiceConfiguration
     private const MODULE_UPDATE_SERVICE_CONFIG = [
 
         '_change_language_with_url_'      =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/ChangeLanguageWithURL']],
+        '_custom_module_manager_'         =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/CustomModuleManager']],
         '_extended_import_export_'        =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/ExtendedImportExport']],
         '_oauth2_client_'                 =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/webtrees-oauth2-client']],
         '_repository_hierarchy_'          =>  ['update_service' => 'GithubModuleUpdate', 'params' => ['github_repo' => 'Jefferson49/RepositoryHierarchy']],
