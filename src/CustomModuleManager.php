9d3b83bfa828dd3e6838f09973713f36a382090b
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-08-17 15:34:51 +0200

    Increased version to 1.0.5

--- src/CustomModuleManager.php
+++ src/CustomModuleManager.php
@@ -92,7 +92,7 @@ class CustomModuleManager extends AbstractModule implements
     use ModuleListTrait;
 
 	//Custom module version
-	public const CUSTOM_VERSION = 'v1.0.4';
+	public const CUSTOM_VERSION = 'v1.0.5';
 
 	//GitHub repository
 	public const GITHUB_REPO = 'Jefferson49/CustomModuleManager';
