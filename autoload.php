2dd204b17142f2180b21428364128aececc0a8a3
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2026-09-19 16:30:29 +0200

    Reverted bugfix autoload

--- autoload.php
+++ autoload.php
@@ -37,8 +37,13 @@ use Composer\Autoload\ClassLoader;
 use League\Flysystem\Filesystem;
 use League\Flysystem\Local\LocalFilesystemAdapter;
 
+
 //Autoload vendor
+//Needs to be autoloaded before the common code library, because otherwise the prepended library will be removed
 require_once __DIR__ . '/vendor/autoload.php';
+
+//Autoload the latest version of the common code library, which is shared between webtrees custom modules
+require_once __DIR__ . '/vendor/jefferson49/webtrees-common/autoload.php';
 
 //Autoload this webtrees custom module
 $loader = new ClassLoader(__DIR__);
